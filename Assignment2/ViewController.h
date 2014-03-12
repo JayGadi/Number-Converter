e18ec2a07eb9029b0653c4cf83d3fac82e0d2354
Jay Gadi <Jay@Jays-Mac-Pro.local>
2014-03-12 03:07:29 -0400

    Initial Commit

---
+++ Assignment2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Assignment2
+//
+//  Created by Jay Gadi on 3/12/14.
+//  Copyright (c) 2014 Jay Gadi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Assignment2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Assignment2
+//
+//  Created by Jay Gadi on 3/12/14.
+//  Copyright (c) 2014 Jay Gadi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
